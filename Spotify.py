2314604ef1f95599f1514a6baa3dfbf531f2a4ec
jaywalkingstreetparking <jjyyrim@gmail.com>
2020-02-24 22:37:36 -0800

    Spotify initial commit.

---
+++ Spotify.py
@@ -0,0 +1,35 @@
+# LIKE YOUTUBE VIDEO TO SPOTIFY
+#
+
+import json
+
+
+
+class Spotify:
+
+    spotify_token = "BQDio-d2m52P3HD6vKS0TKzb5y2_FmGrO8xppWni-iVKlEnVJoZDdUlGYBdLVBpiyfvjMdryzqCz-_CIbytCeS5PCls6hRRa2a6znHAY7kuoNlBfWJ3KoPA0Q8FBMvkyWYAdr4-usri_WomTSEeby1RY8Tbf06WHVLt7W5lZ9fQ"
+    spotify_user_id = "jjyyrim"
+
+    def __init__(self):
+        self.user_id = spotify_user_id
+        self.spo
+    # login to youtube
+    def get_youtube_client(self):
+        pass
+
+    def get_liked_videos(self):
+        pass
+
+    def create_spotify_playlist(self):
+
+        request_body = json.dumps({
+            "name": "youtube likes",
+            "description": "all liked viedos",
+            "public": True
+        })
+
+    def get_spotify_url(self):
+        pass
+
+    def add_song_to_spotify_playlist(self):
+        pass
